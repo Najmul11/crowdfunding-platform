a906d29c231ca3ff41f3b7a30fd420e01598d560
Najmul <najmulbinnurul@gmail.com>
2024-03-12 12:40:34 +0600

    f-edited header

--- frontend/components/layouts/Header.tsx
+++ frontend/components/layouts/Header.tsx
@@ -5,13 +5,16 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="py-4 sticky top-0">
-      <div className="max-w-screen-xl mx-auto p-2  rounded bg-gradient-to-r from-slate-800 to-blue-500 from-[10%] ">
-        <div className="flex  justify-between items-center">
-          <Link href={"/"}>
-            <Image src={logo} width={50} height={50} alt="" />
+      <div className="max-w-screen-xl mx-auto p-4  rounded bg-gradient-to-r from-slate-800 to-blue-500 from-[10%] ">
+        <div className="flex  justify-between items-center ">
+          <Link
+            href={"/"}
+            className="font-bold uppercase text-2xl bg-clip-text text-transparent bg-gradient-to-r from-highlight to-white"
+          >
+            Fund Forge
           </Link>
 
-          <div className="flex gap-5 items-center text-white font-semibold text-sm pr-2">
+          <div className="flex gap-5 items-center text-white font-semibold text-sm ">
             <Link
               href={"/how-it-works"}
               className="hover:text-highlight duration-300"
@@ -25,9 +28,15 @@ const Header = () => {
               About us
             </Link>
 
-            <button className="rounded bg-black px-5 py-2 text-highlight hover:bg-highlight hover:text-black duration-300">
-              Donate now
-            </button>
+            <div className="flex gap-1 items-center">
+              <button className="rounded bg-blue-500/50 px-5 py-2 text-highlight hover:bg-highlight hover:text-black duration-300">
+                Create campaign
+              </button>
+
+              <button className="rounded bg-black px-5 py-2 text-highlight hover:bg-highlight hover:text-black duration-300">
+                Donate now
+              </button>
+            </div>
           </div>
         </div>
       </div>

ecd6468f436eedb0d1227ce2b503aff226fcfeb5
Najmul <najmulbinnurul@gmail.com>
2024-03-15 22:23:26 +0600

    f-loading state, no data

--- frontend/components/home-page/campaigns/Campaings.tsx
+++ frontend/components/home-page/campaigns/Campaings.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { useContractRead, useContract } from "@thirdweb-dev/react";
 import Card from "./Card";
+
+export type TCampaign = (string | number | boolean)[];
+
 const Campaings = () => {
   const { contract } = useContract(
     "0xBA69a8fcA948270b18d268e248FF53971bEC4f0E"
@@ -9,12 +12,34 @@ const Campaings = () => {
   console.log(data);
 
   return (
-    <div className="grid grid-cols-3 gap-5">
-      {data && data.length > 0
-        ? data.map((campaign, i: number) => (
-            <Card campaign={campaign} key={i} />
-          ))
-        : ""}
+    <div>
+      <div className="flex">
+        <h3 className="text-xl font-bold bg-blue-500/30 py-1 px-5 rounded-full">
+          Live Campaigns
+        </h3>
+      </div>
+
+      {isLoading ? (
+        <></>
+      ) : (
+        <>
+          {data && data.length > 0 ? (
+            <div className="grid grid-cols-3 gap-5 ">
+              {data.map((campaign: TCampaign, i: number) => (
+                <>
+                  <Card campaign={campaign} key={i} />
+                  <Card campaign={campaign} key={i} />
+                  <Card campaign={campaign} key={i} />
+                </>
+              ))}
+            </div>
+          ) : (
+            <p className="mt-10 rounded-xl  text-center font-semibold py-10 bg-red-400/20">
+              No campaign running at the moment❌❌❌
+            </p>
+          )}
+        </>
+      )}
     </div>
   );
 };
--- frontend/components/home-page/campaigns/Card.tsx
+++ frontend/components/home-page/campaigns/Card.tsx
@@ -1,22 +1,20 @@
 "use client";
 import Image from "next/image";
-import Link from "next/link";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
-import { TCampaign } from "@/lib/types/campaign";
+import { TCampaign } from "./Campaings";
 
-const Card = ({ campaign }) => {
-  const { banner, deadline, description, target, title } = campaign;
-  console.log(campaign);
-
+const Card = ({ campaign }: { campaign: TCampaign }) => {
   return (
     <CardContainer className="inter-var">
-      <CardBody className="bg-blue-500/70 relative group/cardw-auto  h-auto rounded-xl p-6 text-second  ">
+      <CardBody className="bg-blue-500/30 relative group/cardw-auto  h-auto rounded-xl p-6 text-second  ">
         <CardItem translateZ="50" className="text-xl font-bold ">
           {campaign[1]}
         </CardItem>
+
         <CardItem as="p" translateZ="60" className=" text-sm max-w-sm mt-2 ">
-          Hover over this card to unleash the power of CSS perspective
+          {campaign[3]}
         </CardItem>
+
         <CardItem translateZ="100" className=" mt-4">
           <Image
             src={
@@ -28,13 +26,16 @@ const Card = ({ campaign }) => {
             alt="thumbnail"
           />
         </CardItem>
+
         <div className="flex justify-end items-center mt-10">
           <CardItem
             translateZ={20}
             as="button"
-            className="px-4 py-2 rounded-xl bg-black   text-white text-xs font-bold"
+            className="rounded-xl font-bold"
           >
-            Sign up
+            <button className="rounded text-xs bg-black px-5 py-2 text-highlight hover:bg-highlight hover:text-black duration-300">
+              Donate
+            </button>
           </CardItem>
         </div>
       </CardBody>
